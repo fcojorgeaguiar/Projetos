8f673b120465253dc7d50d59fafe855074322558
fcojorgeaguiar <fcojorgeaguiar@gmail.com>
2025-03-01 13:11:43 -0300

    Criei relogio

---
+++ Relogio/script.js
@@ -0,0 +1,33 @@
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date()
+    let h = dateToday.getHours()
+    let m = dateToday.getMinutes()
+    let s = dateToday.getSeconds()
+    
+    if(h < 10){h = '0'+h}
+    if(m < 10){m = '0'+m}
+    if(s < 10){s = '0'+s}
+
+    horas.textContent = h
+    minutos.textContent = m
+    segundos.textContent = s
+
+
+
+})
+
+function manha(){
+    document.body.style.background = 'linear-gradient(120deg, #027333, #04D939)'
+}
+
+function tarde(){
+    document.body.style.background = ' background: linear-gradient(120deg, #ffe53bd8, #ff2525da);'
+}
+
+function noite(){
+    document.body.style.background = 'linear-gradient(120deg, #024059, #026873)'
+}
