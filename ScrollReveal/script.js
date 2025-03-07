a337c3e9f14b642aa86992bacd492b0bf7d873c4
fcojorgeaguiar <fcojorgeaguiar@gmail.com>
2025-03-07 14:12:35 -0300

    criei Scroll Reveal

---
+++ ScrollReveal/script.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({resert:true})
+
+sr.reveal('.title', {
+     rotate: {x: 100, y: 100, z: 0},
+     duration: 2000
+})
+
+sr.reveal('.p', {
+    rotate: {x:0, y: 80, z:0},
+    duration: 2000
+})
